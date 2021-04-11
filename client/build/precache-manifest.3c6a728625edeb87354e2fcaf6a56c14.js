12d831080d638e8f41a1d11c7af675abcafb19fb
Hariharan <harigates123456@gmail.com>
2021-04-11 15:57:16 +0530

    first commit

---
+++ client/build/precache-manifest.3c6a728625edeb87354e2fcaf6a56c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885b336b854ef212cfb2cd1053d2ff7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c08c1cdbfadc737b23",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c08c1cdbfadc737b23",
+    "url": "/static/js/main.0156cfca.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
